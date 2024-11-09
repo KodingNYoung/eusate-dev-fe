4eff8b093043af0627e34edcc9258db495c457d6
KodingNYoung <abiodunadebambo44@gmail.com>
2024-11-09 23:42:38 +0100

    chore:wrote tests for auth header component

--- src/components/molecules/AuthHeader.tsx
+++ src/components/molecules/AuthHeader.tsx
@@ -1,24 +1,30 @@
 "use client"
 
 import { FC } from "@/utils/types"
-import React, { ReactNode } from "react"
+import React, { HTMLProps, ReactNode } from "react"
 import Button from "./Buttons"
 import Icon from "../atoms/Icon"
 import Typography from "../atoms/Typography"
 import { useRouter } from "next/navigation"
 import Toast from "../organisms/Toast"
 
-type Props = {
+type Props = Omit<HTMLProps<HTMLDivElement>, "title"> & {
   hasBackBtn?: boolean
   title: ReactNode
   subtitle: ReactNode
   toast?: boolean
 }
 
-const AuthHeader: FC<Props> = ({ hasBackBtn, title, subtitle, toast }) => {
+const AuthHeader: FC<Props> = ({
+  hasBackBtn,
+  title,
+  subtitle,
+  toast,
+  ...props
+}) => {
   const router = useRouter()
   return (
-    <>
+    <header {...props}>
       {hasBackBtn && (
         <Button
           variant="tetiary"
@@ -31,19 +37,17 @@ const AuthHeader: FC<Props> = ({ hasBackBtn, title, subtitle, toast }) => {
         </Button>
       )}
       {toast && <Toast />}
-      <header>
-        <Typography as="h2" className="text-bold-2xl sm:text-bold-4xl mb-3">
-          {title}
-        </Typography>
-        <Typography
-          as="span"
-          variant="regular-sm"
-          className="text-gray-500 flex items-center gap-2"
-        >
-          {subtitle}
-        </Typography>
-      </header>
-    </>
+      <Typography as="h2" className="text-bold-2xl sm:text-bold-4xl mb-3">
+        {title}
+      </Typography>
+      <Typography
+        as="span"
+        variant="regular-sm"
+        className="text-gray-500 flex items-center gap-2"
+      >
+        {subtitle}
+      </Typography>
+    </header>
   )
 }
 
